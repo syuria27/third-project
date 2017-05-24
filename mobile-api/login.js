14892cb616bf595d2cbba3ac6fb2bca54fbfeb1a
Syukron Rizqi Ardhi <syukron.tkj2@gmail.com>
2017-05-24 17:51:37 +0700

    Add login and server

---
+++ mobile-api/login.js
@@ -0,0 +1,63 @@
+var mysql = require("mysql");
+const isset = require('isset');
+const md5 = require("md5");
+
+function LOGIN_ROUTER(router, pool) {
+    var self = this;
+    self.handleRoutes(router, pool);
+}
+
+LOGIN_ROUTER.prototype.handleRoutes = function (router, pool) {
+
+    router.post("/login", function (req, res) {
+        var data = {
+            error: true,
+            error_msg: ""
+        };
+
+        if (isset(req.body.username) && isset(req.body.password)) {
+            var query = `SELECT u.kode_sales,nama_sales,depot,password 
+                        FROM user u LEFT JOIN login l ON u.kode_sales = l.kode_sales 
+	         			WHERE u.kode_sales = ? AND u.status = 1 AND l.hak_akses = 1`;
+            var table = [req.body.username];
+            query = mysql.format(query, table);
+            pool.getConnection(function (err, connection) {
+                connection.query(query, function (err, rows) {
+                    connection.release();
+                    if (err) {
+                        res.status(500);
+                        data.error_msg = "Error executing MySQL query";
+                        res.json(data);
+                    } else {
+                        if (rows.length != 0) {
+                            if (rows[0].password == md5(req.body.password)) {
+                                data.error = false;
+                                data.error_msg = 'Success..';
+                                data.user = {
+                                    kode_sales: rows[0].kode_sales,
+                                    nama_sales: rows[0].nama_sales,
+                                    depot: rows[0].depot,
+                                };
+                                res.json(data);
+                            } else {
+                                data.error_msg = 'Login fail check password..';
+                                res.status(403);
+                                res.json(data);
+                            }
+                        } else {
+                            data.error_msg = 'No users Found..';
+                            res.status(404);
+                            res.json(data);
+                        }
+                    }
+                });
+            });
+        } else {
+            data.error_msg = 'Missing some params..';
+            res.status(400);
+            res.json(data);
+        }
+    });
+}
+
+module.exports = LOGIN_ROUTER;
---
+++ mobile-api/server.js
@@ -0,0 +1,61 @@
+var express = require("express");
+var morgan = require("morgan");
+var cors = require("cors");
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
+
+const login = require("./login.js");
+const visit = require("./visit.js");
+
+var app = express();
+
+function REST() {
+    var self = this;
+    self.connectMysql();
+};
+
+REST.prototype.connectMysql = function () {
+    var self = this;
+    var pool = mysql.createPool({
+        connectionLimit: 50,
+        waitForConnection: true,
+        host: '192.168.1.2',
+        user: 'syuria',
+        password: '20juli95',
+        database: 'npsales',
+        debug: false
+    });
+    self.configureExpress(pool);
+}
+
+REST.prototype.configureExpress = function (pool) {
+    var self = this;
+    app.use(cors());
+    app.use(morgan("combined"));
+    app.use('/selfie', express.static('upload'));
+    app.use(bodyParser.urlencoded({ limit: "50mb",extended: true }));
+    app.use(bodyParser.json({limit: "50mb"}));
+    var router = express.Router();
+    app.use('/api', router);
+    var login_router = new login(router, pool);
+    var visit_router = new visit(router, pool);
+    // Handle 404 - Keep this as a last route
+    app.use(function (req, res, next) {
+        res.status(400);
+        res.json({ "error": true, "error_msg": "Method Not Found" });
+    });
+    self.startServer();
+}
+
+REST.prototype.startServer = function () {
+    app.listen(3002, function () {
+        console.log("All right ! I am alive at Port 3002.");
+    });
+}
+
+REST.prototype.stop = function (err) {
+    console.log("ISSUE WITH MYSQL n" + err);
+    process.exit(1);
+}
+
+new REST();
